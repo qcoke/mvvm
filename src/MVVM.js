39ea72e3b309d1307e71c2bdc97ff794177de8d4
可乐加糖 <33370733@qq.com>
2020-02-26 10:58:53 +0800

    开始学习自写 Vue

--- src/MVVM.js
+++ src/MVVM.js
@@ -1,22 +1,24 @@
-class QVE {
+class MVVM {
   // 构造器
-  constructor (options) {
+  constructor(options) {
+    // 把EL,DATA挂在在MVVM中
     this.$el = options.el;
     this.$data = options.data;
     // 如果有要编译的模板就开始编译
-    if(this.$el) {
+    if (this.$el) {
       // 数据劫持，就是把对象所有的属性添加 get 和 set
       new Observer(this.$data);
       // 将数据代理到实例上
       this.proxyData(this.$data);
       // 用数据和元素进行编译
-      new Compiler(this.el,this);
+      new Compiler(this.el, this);
     }
   }
   // 代理数据的方法
-  proxyData(data){
+  proxyData(data) {
     Object.keys(data).forEach(key => {
-      Object.defineProperty(this,key, {
+      // 如果有子属性，也添加订阅（不过感觉这个东西不是一个深度迭代的，就迭代了第一层）
+      Object.defineProperty(this, key, {
         get() {
           return data[key];
         },
--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import _ from 'lodash';
 import './assets/css/screen.css';
 
-let data = {text:'world'};
+let data = { text: 'world' };
 let val = data.text;
 
-function observe(data) {
+/* function observe(data) {
     // 监听的 DATA 对象
-    if(Object.prototype.toString.call(data) === '[object Object]') {
-        for(let prop in data) {
-            defineReactive(data, prop,data[prop]);
+    if (Object.prototype.toString.call(data) === '[object Object]') {
+        for (let prop in data) {
+            defineReactive(data, prop, data[prop]);
         }
     }
-}
-
-
+} */
 
 Reflect.defineProperty(data, 'text', {
-    get(){
+    get() {
         console.log('get:' + val);
         return val;
     },
@@ -26,10 +24,9 @@ Reflect.defineProperty(data, 'text', {
     }
 });
 
-window.onload = function(){
+window.onload = function () {
     var btn = document.getElementById("btn");
-    btn.addEventListener("click", function(){
-        // alert('x');
+    btn.addEventListener("click", function () {
         data.text = "vue";
         console.log("===>", data.text);
         console.log("改一个不存在的对象试试，应该到这里就是最后一条输出语句了。")
